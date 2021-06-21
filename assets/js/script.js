83d0b6ba691a0f0e10c3476810df807fb1e7bbab
fshrabon <fshrabon151@gmail.com>
2021-06-21 10:19:09 +0600

    wow js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,15 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+wow = new WOW(
+  {
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true ,
+
+}
+)
+wow.init();
